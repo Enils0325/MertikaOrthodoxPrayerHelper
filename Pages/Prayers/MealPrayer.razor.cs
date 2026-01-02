8712cc17f74efb20ddd42c4dbce8c83aaf1d5754
Nils <nils0325@outlook.com>
2026-01-02 15:06:23 -0500

    moved parameters

--- Pages/Prayers/MealPrayer.razor.cs
+++ Pages/Prayers/MealPrayer.razor.cs
@@ -4,11 +4,5 @@ namespace OrthodoxPrayerBlazorSite2.Pages.Prayers;
 
 public partial class MealPrayer
 {
-    [Parameter]
-    public MealKindEnum MealKind { get; set; } = MealKindEnum.Breakfast;
-
-    [Parameter]
-    public BeforeAfterKindEnum BeforeAfterKind { get; set; } = BeforeAfterKindEnum.Before;
-
     private string getBeforeAfterText() => BeforeAfterKind == BeforeAfterKindEnum.Before ? "before" : "after";
 }
