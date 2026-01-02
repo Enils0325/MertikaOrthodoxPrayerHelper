23fe41010e24251c8c1199614c17bcbac81cda46
Nils <nils0325@outlook.com>
2026-01-01 20:00:00 -0500

    Removed bootstrap. Added ability to select prayers

---
+++ Models/StaticColors.cs
@@ -0,0 +1,7 @@
+﻿namespace OrthodoxPrayerBlazorSite2.Models;
+
+public static class StaticColors
+{
+    public const string DarkRed = "#8B0000";
+    public const string LightBeige = "#F5F5DC";
+}
---
+++ Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace OrthodoxPrayerBlazorSite2.Pages;
+
+public partial class Home
+{
+    private DateTime Date { get; set; } = DateTime.Now;
+    private MainPrayerChoiceKind MainPrayerChoice { get; set; } = MainPrayerChoiceKind.None;
+
+}
+
+public enum MainPrayerChoiceKind
+{
+    None,
+    BeforeMeal,
+    AfterMeal,
+    MorningPrayer,
+    EveningPrayer,
+}
