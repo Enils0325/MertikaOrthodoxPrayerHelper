1075414b6d2b20426aa8439adba71bdfe5d811e8
Nils <nils0325@outlook.com>
2026-03-13 23:55:15 -0400

    loading container changes

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -2,10 +2,12 @@
 
 namespace OrthodoxPrayerBlazorSite2.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject]
     public NilsEventService NilsEventService { get; set; } = null!;
+
+    private bool IsLoading { get; set; } = false;
 
     private IEnumerable<MainPrayerChoiceKind> choiceKindsWithNoRandom = [
         MainPrayerChoiceKind.Other,
@@ -14,4 +16,26 @@ public partial class Home
 
     private DateTime Date { get; set; } = DateTime.Now;
     private MainPrayerChoiceKind MainPrayerChoice { get; set; } = MainPrayerChoiceKind.None;
+
+    protected override async Task OnInitializedAsync()
+    {
+        NilsEventService.OnRandomClicked += NilsEventService_OnRandomClicked;
+        await base.OnInitializedAsync();
+    }
+
+    private async Task NilsEventService_OnRandomClicked()
+    {
+        IsLoading = true;
+        StateHasChanged();
+
+        await Task.Delay(500);
+
+        IsLoading = false;
+        StateHasChanged();
+    }
+
+    public void Dispose()
+    {
+        NilsEventService.OnRandomClicked -= NilsEventService_OnRandomClicked;
+    }
 }
---
+++ Utils/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace OrthodoxPrayerBlazorSite2.Utils;
+
+public static class Extensions
+{
+    public static DateTime ToDateOnlyAtTime0(this DateOnly dateOnly)
+    {
+        return new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day);
+    }
+}
