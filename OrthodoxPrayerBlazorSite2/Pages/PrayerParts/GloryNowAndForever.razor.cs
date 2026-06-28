4cee9a89563b291ca527371c2ff2624f22be858f
Nils <nils0325@outlook.com>
2026-06-28 17:33:25 -0400

    Precommunion prayers added as expanded

---
+++ OrthodoxPrayerBlazorSite2/Pages/PrayerParts/GloryNowAndForever.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OrthodoxPrayerBlazorSite2.Pages.PrayerParts;
+
+public partial class GloryNowAndForever
+{
+    [Parameter]
+    public GloryNowOptionKind GloryNowOption { get; set; } = GloryNowOptionKind.All;
+}
+
+public enum GloryNowOptionKind
+{
+    All,
+    FirstPortion,
+    LastPortion,
+}
